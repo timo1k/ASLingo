8af3b8ffa252e9760f4c38ea0abbce4c31e64e75
Timothy <timkhumpan@gmail.com>
2024-02-26 10:09:05 -0500

    Initial Commit

---
+++ suiCam/suiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiCamApp.swift
+//  suiCam
+//
+//  Created by Timothy Khumpan on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct suiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
